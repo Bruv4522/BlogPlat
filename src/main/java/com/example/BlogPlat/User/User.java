9010045c09e2863893a74f1fc2a43d6c261b3235
BluesySugar4522real <bluesysugar4522@gmail.com>
2026-09-12 13:07:42 +0530

    Created User model

---
+++ src/main/java/com/example/BlogPlat/User/User.java
@@ -0,0 +1,20 @@
+package com.example.BlogPlat.User;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, length = 25, nullable = false)
+    private String username;
+    @Column(unique = true, length = 25, nullable = false)
+    private String password;
+}
